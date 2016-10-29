fa5aebb85a90580e1efce6ff6c43a836b5a4d789
Robert Wagner <tholos@gmail.com>
2016-10-29 04:17:55 -0400

    Update import.sh

--- backendData/import.sh
+++ backendData/import.sh
@@ -1,3 +1,5 @@
 mongoimport --db test --collection indicators --drop --file ./indicators.txt 
 mongoimport --db test --collection sections --drop --file ./sections.txt 
 mongoimport --db test --collection questions --drop --file ./questions.txt 
+mongoimport --db test --collection users --drop --file ./users.txt
+mongoimport --db test --collection meta --drop --file ./meta.txt
