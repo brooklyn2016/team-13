742440a8b4f5befab7bbaf4fab9f8d2415ff8e2d
Robert Wagner <tholos@gmail.com>
2016-10-29 01:48:21 -0400

    Create import.sh
    add script to import data

---
+++ data/import.sh
@@ -0,0 +1,3 @@
+mongoimport --db test --collection indicators --drop --file ./indicators.txt 
+mongoimport --db test --collection sections --drop --file ./sections.txt 
+mongoimport --db test --collection questions --drop --file ./questions.txt 
