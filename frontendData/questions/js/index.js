b138f35a0fa6722188258929c052ad37530f24f1
Matthew Griffin <grffn.matt@gmail.com>
2016-10-29 04:24:00 -0400

    Add files via upload

---
+++ frontendData/questions/js/index.js
@@ -0,0 +1,9 @@
+var server ='ec2-107-20-10-161.compute-1.amazonaws.com';
+
+var resQuestions = '/questions';
+
+var resAssessments = '/assessments';
+
+var resUsers = '/users';
+
+var resReports = '/reports';
