650baeb925dacd9383b4c0d8e6b698bc94ecf73e
Force <force@ip-172-31-11-233.ec2.internal>
2016-10-29 13:29:11 +0000

    server backup

--- frontendData/questions/js/index.js
+++
@@ -1,9 +0,0 @@
-var server ='ec2-107-20-10-161.compute-1.amazonaws.com';
-
-var resQuestions = '/questions';
-
-var resAssessments = '/assessments';
-
-var resUsers = '/users';
-
-var resReports = '/reports';
